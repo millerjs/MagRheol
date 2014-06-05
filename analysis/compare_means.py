5108e88dbfb3485caef847b54df3f17600680dc6
Joshua Miller <jshuasmiller@gmail.com>
2014-06-05 08:06:04 -0500

    gathering rate data

--- analysis/compare_means.py
+++ analysis/compare_means.py
@@ -4,21 +4,23 @@ from numpy import *
 
 k = 13
 
-path = "h1.dat"
+path = "h_15.dat"
 
 data = array(plt.parse(path))
 
-f = lambda x, a, b, c:  a*exp(-x*b) + c
-
-p0 = [.35, 1, .05]
-p, e = optimization.curve_fit(f, data[:,0], data[:,1], p0)
-print p
+f = lambda x, a, b, c, d: a*exp(-b*x**c) + d
 
 fig, ax = plt.new_plot(title="K-means, %d Clusters" % k,
                        xaxis="Time [ms]",
                        yaxis="K-means distortion")
 
+p0 = [2.7, 1., 2.1, .25]
+p, e = optimization.curve_fit(f, data[:,0], data[:,1], p0)
+
+print p
+
+
 X = linspace(0, max(data[:,0]), 100)
-plt.dot(data[:,0], data[:,1], "H = 100 mT")
-plt.add(X, f(X, p[0], p[1], p[2]), "Exponential fit")
-plt.save_plot(fig, ax, "plt3.png")
+plt.dot(data[:,0]*.001, data[:,1], "H = 100 mT")
+plt.add(X*.001, f(X, p[0], p[1], p[2], p[3]), "Exponential fit")
+plt.save_plot(fig, ax, path[:-4]+".png")
